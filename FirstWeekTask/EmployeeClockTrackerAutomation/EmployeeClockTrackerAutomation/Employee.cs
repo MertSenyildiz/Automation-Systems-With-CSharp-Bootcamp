e917cc92e75fed6990d8b84bbe30ab90750bff2d
MertSenyildiz <mert.senyildiz123@gmail.com>
2023-09-16 23:28:15 +0300

    initial commit

---
+++ FirstWeekTask/EmployeeClockTrackerAutomation/EmployeeClockTrackerAutomation/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeClockTrackerAutomation
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double HourlyPay { get; set; }
+
+        public List<Shift> Shifts { get; set; } = new();
+
+    }
+}
---
+++ FirstWeekTask/EmployeeClockTrackerAutomation/EmployeeClockTrackerAutomation/EmployeeManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeClockTrackerAutomation
+{
+    
+    public class EmployeeManager
+    {
+        EmployeeDal _employeeDal;
+        public EmployeeManager(EmployeeDal employeeDal)
+        {
+            _employeeDal = employeeDal;
+        }
+
+        public Employee GetEmployeeRecords(int id)
+        {
+            return _employeeDal.GetById(id);
+        }
+        public List<Employee> GetAllEmployeeRecords()
+        {
+            return _employeeDal.GetAll();
+        }
+
+        public List<Shift> GetEmployeeShifts(int id)
+        {
+            return _employeeDal.GetById(id).Shifts;
+        }
+
+        public List<double> GetEmployeeOverTimeShifts(int id)
+        {
+            return _employeeDal.GetById(id).Shifts.Select(s=>(s.End-s.Start).TotalHours).ToList();
+        }
+    }
+}
